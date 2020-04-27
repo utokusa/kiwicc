9326fd53799b27e24b8c96d3b2d83c9f46cbeced
utokusa <utokusa@gmail.com>
2020-04-28 00:22:54 +0900

    Reimplement the parser using recursive descending parsing

--- 9cc/9cc.c
+++ 9cc/9cc.c
@@ -22,6 +22,26 @@ struct Token
   char *str;      // Token string
 };
 
+// Kind of AST node
+typedef enum
+{
+  ND_ADD, // +
+  ND_SUB, // -
+  ND_MUL, // *
+  ND_DIV, // /
+  ND_NUM, // integer
+} NodeKind;
+
+// Node of AST
+typedef struct Node Node;
+struct Node
+{
+  NodeKind kind;
+  Node *lhs;
+  Node *rhs;
+  int val; // Use only if kind is ND_NUM
+};
+
 // Current token
 Token *token;
 
@@ -135,6 +155,65 @@ Token *tokenize()
   return head.next;
 }
 
+Node *new_node(NodeKind kind, Node *lhs, Node *rhs)
+{
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = kind;
+  node->lhs = lhs;
+  node->rhs = rhs;
+  return node;
+}
+
+Node *new_node_num(int val)
+{
+  Node *node = calloc(1, sizeof(Node));
+  node->kind = ND_NUM;
+  node->val = val;
+  return node;
+}
+
+Node *expr()
+{
+  Node *node = new_node_num(expect_number());
+
+  for (;;)
+  {
+    if (consume('+'))
+      node = new_node(ND_ADD, node, new_node_num(expect_number()));
+    else if (consume('-'))
+      node = new_node(ND_SUB, node, new_node_num(expect_number()));
+    else
+      return node;
+  }
+}
+
+void gen(Node *node)
+{
+  if (node->kind == ND_NUM)
+  {
+    printf("  push %d\n", node->val);
+    return;
+  }
+
+  gen(node->lhs);
+  gen(node->rhs);
+
+  printf("  pop rdi\n");
+  printf("  pop rax\n");
+
+  switch (node->kind)
+  {
+  case ND_ADD:
+    printf("  add rax, rdi\n");
+    break;
+  case ND_SUB:
+    printf("  sub rax, rdi\n");
+    break;
+  }
+
+  printf("  push rax\n");
+}
+
 int main(int argc, char **argv)
 {
   if (argc != 2)
@@ -146,26 +225,16 @@ int main(int argc, char **argv)
   // Tokenize
   user_input = argv[1];
   token = tokenize();
+  Node *node = expr();
 
   // Output the assembly code.
   printf(".intel_syntax noprefix\n");
   printf(".global main\n");
   printf("main:\n");
 
-  printf("  mov rax, %d\n", expect_number());
+  gen(node);
 
-  while (!at_eof())
-  {
-    if (consume('+'))
-    {
-      printf("  add rax, %d\n", expect_number());
-      continue;
-    }
-
-    expect('-');
-    printf("  sub rax, %d\n", expect_number());
-  }
-
+  printf("  pop rax\n");
   printf("  ret\n");
   return 0;
 }
