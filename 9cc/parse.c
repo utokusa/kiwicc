15ddc71b4b1307f25cf0e7886403a867c604f214
utokusa <utokusa@gmail.com>
2020-05-05 00:07:27 +0900

    Fix a comment on stmt()

--- 9cc/parse.c
+++ 9cc/parse.c
@@ -142,7 +142,7 @@ static Function *function()
 
 // stmt = expr ";"
 //      | "{" stmt* "}"
-//      | "if" "(" expr ")" stmt ("else if" "(" expr ")" stmt)* ("else" stmt)?
+//      | "if" "(" expr ")" stmt ("else" stmt)?
 //      | "while" "(" expr ")" stmt
 //      | "for" "(" expr? ";" expr? ";" expr? ")" stmt
 //      | "return" expr ";"
